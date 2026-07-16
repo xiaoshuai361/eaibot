4f217296701750c897fe5d24b1fde9617073752a
xiaoshuai361 <xiaoshuai361@github.com>
2026-07-16 10:37:15 +0800

    chore: preserve pre-block-grasp arm script

---
+++ handeye-calib/tests/conftest.py
@@ -0,0 +1,9 @@
+from __future__ import absolute_import
+
+import os
+import sys
+
+
+SRC_DIR = os.path.abspath(os.path.join(os.path.dirname(__file__), "..", "src"))
+if SRC_DIR not in sys.path:
+    sys.path.insert(0, SRC_DIR)
